ad90474ea178c1159846090160cdd41af40afe20
BinZ <sikatunar@gmail.com>
2023-11-02 23:38:29 +0800

    fix: animation

--- components/ui/phase-roadmap.tsx
+++ components/ui/phase-roadmap.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { cn } from '@/lib/utils';
-import { titilium } from '@/public/fonts';
+import React from "react";
+import { cn } from "@/lib/utils";
+import { titilium } from "@/public/fonts";
 
 interface IPhaseRoadmap {
   number: string;
@@ -10,16 +10,21 @@ interface IPhaseRoadmap {
 
 const PhaseRoadmap = ({ number, bgcolor, className }: IPhaseRoadmap) => {
   return (
-    <div className='flex flex-col items-center max-w-[100px] w-full grow-0'>
-      <div className='flex flex-col items-center gap-2  w-full'>
-        <div className={`${className} relative`}>
-          <h1 className={cn(titilium.className, 'text-[0.8rem] font-semibold text-alice-white/50')}>
+    <div className="flex flex-col items-center max-w-[100px] w-full grow-0">
+      <div className="flex flex-col items-center gap-2  w-full">
+        <div className={`${className} relative duration-1000`}>
+          <h1
+            className={cn(
+              titilium.className,
+              "text-[0.8rem] font-semibold text-alice-white/50"
+            )}
+          >
             Phase
           </h1>
           <h1
             className={cn(
               titilium.className,
-              `text-[0.8rem] font-semibold blur-xl text-transparent absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${bgcolor}`,
+              `text-[0.8rem] font-semibold blur-xl text-transparent absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${bgcolor}`
             )}
           >
             Phase
@@ -27,12 +32,17 @@ const PhaseRoadmap = ({ number, bgcolor, className }: IPhaseRoadmap) => {
         </div>
 
         <h1
-          className={(cn(titilium.className), 'text-[2.44125rem] font-semibold text-alice-white')}
+          className={
+            (cn(titilium.className),
+            "text-[2.44125rem] font-semibold text-alice-white")
+          }
         >
           {number}
         </h1>
       </div>
-      <div className={`duration-1000 delay-150 ${bgcolor} w-[4px] z-[1] block faded-corners`} />
+      <div
+        className={`duration-1000 delay-150 ${bgcolor} w-[4px] z-[1] block faded-corners`}
+      />
     </div>
   );
 };
