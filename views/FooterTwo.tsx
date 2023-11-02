df4638a590d60493aa65348275cb9a9f4bfb08a4
BinZ <sikatunar@gmail.com>
2023-11-03 00:56:14 +0800

    Update FooterTwo.tsx

--- views/FooterTwo.tsx
+++ views/FooterTwo.tsx
@@ -66,7 +66,7 @@ const FooterTwo = () => {
 
       <div className="flex justify-center items-center grow bg-alice-white">
         <div className="flex flex-wrap justify-center xl:justify-between items-center py-8 px-4 xl:px-0 grow max-w-[90rem]">
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 ">
             <h1
               className={cn(
                 titilium.className,
