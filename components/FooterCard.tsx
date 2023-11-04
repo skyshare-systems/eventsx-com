ef609a235fe8c4e08bd938f1999a2bd188ab1f2e
BinZ <sikatunar@gmail.com>
2023-11-04 13:38:49 +0800

    fix: footer

--- components/FooterCard.tsx
+++ components/FooterCard.tsx
@@ -1,6 +1,6 @@
-import { titilium } from '@/public/fonts';
-import React from 'react';
-import { cn } from '@/lib/utils';
+import { titilium } from "@/public/fonts";
+import React from "react";
+import { cn } from "@/lib/utils";
 
 interface IFooterData {
   title: string;
@@ -9,27 +9,33 @@ interface IFooterData {
 
 const FooterCard = ({ title, object }: IFooterData) => {
   return (
-    <div className='flex flex-col gap-5'>
-      <h1 className={cn(titilium.className, 'text-base font-semibold text-alice-white')}>
+    <div className="flex flex-col gap-5">
+      <h1
+        className={cn(
+          titilium.className,
+          "text-base font-semibold text-alice-white"
+        )}
+      >
         {title}
       </h1>
-      <div className='flex flex-col gap-4'>
+      <div className="flex flex-col gap-4">
         {object.map((data: any, index: any) => {
           return (
             <a
               key={index}
+              href={data.linkUrl}
               className={cn(
                 titilium.className,
-                'text-base font-semibold text-alice-white/70 cursor-pointer hover:opacity-50 duration-150 flex items-center gap-[10px]',
+                "text-base font-semibold text-alice-white/70 cursor-pointer hover:opacity-50 duration-150 flex items-center gap-[10px]"
               )}
             >
               {data.name}
-              {data.name === 'Status' && (
-                <div className='rounded-full bg-[#18D718] py-1 px-2 animate-pulse'>
+              {data.name === "Status" && (
+                <div className="rounded-full bg-[#18D718] py-1 px-2 animate-pulse">
                   <p
                     className={cn(
                       titilium.className,
-                      'text-base font-normal text-alice-white/70 uppercase',
+                      "text-base font-normal text-alice-white/70 uppercase"
                     )}
                   >
                     Live
