a8f00b2f67b85c2b49e985bbcd12570b1af8b323
BinZ <sikatunar@gmail.com>
2023-11-06 23:04:31 +0800

    fix: added in team section

--- views/Team.tsx
+++ views/Team.tsx
@@ -6,6 +6,7 @@ import FrameOne from "@/public/assets/team/frame-1.svg";
 import FrameTwo from "@/public/assets/team/frame-2.svg";
 import FrameThree from "@/public/assets/team/frame-3.svg";
 import FrameFour from "@/public/assets/team/frame-4.svg";
+import Image from "next/image";
 
 const Team = () => {
   return (
@@ -21,31 +22,105 @@ const Team = () => {
         </h2>
 
         <div className="relative w-[1160px] h-[818px] hidden xl:block">
+          <FrameOne className="absolute top-0 left-0" />
           <a
             href="https://www.linkedin.com/in/dash-manikkuwadura/"
             target="_blank"
+            className={cn(
+              titilium.className,
+              "flex items-center gap-1 z-[2]",
+              "absolute top-[45%] left-[40%] transform -translate-x-[40%] -translate-y-[45%] group opacity-50 hover:opacity-100 duration-150"
+            )}
           >
-            <FrameOne className="absolute top-0 left-0 hover:brightness-125 duration-150" />
+            <Image
+              src={"/assets/team/linkedin-icon.png"}
+              alt={"LinkedIn"}
+              height={18.75}
+              width={18.75}
+            />{" "}
+            LinkedIn
+            <Image
+              src={"/assets/team/arrow-right.png"}
+              alt={"LinkedIn"}
+              height={18.75}
+              width={18.75}
+              className="ml-1 group-hover:ml-3 duration-300"
+            />{" "}
           </a>
+          <FrameTwo className="absolute -top-[4.7%] right-0 hover:brightness-125 duration-150" />
           <a
             href="https://www.linkedin.com/in/shoaib-aslam-aca-fmaat-13055319/"
             target="_blank"
+            className={cn(
+              titilium.className,
+              "flex items-center gap-1 z-[2]",
+              "absolute top-[50%] right-[2%] transform -translate-x-[2%] -translate-y-[50%] group opacity-50 hover:opacity-100 duration-150"
+            )}
           >
-            <FrameTwo className="absolute -top-[4.7%] right-0 hover:brightness-125 duration-150" />
+            <Image
+              src={"/assets/team/linkedin-icon.png"}
+              alt={"LinkedIn"}
+              height={18.75}
+              width={18.75}
+            />{" "}
+            LinkedIn
+            <Image
+              src={"/assets/team/arrow-right.png"}
+              alt={"LinkedIn"}
+              height={18.75}
+              width={18.75}
+              className="ml-1 group-hover:ml-3 duration-300"
+            />{" "}
           </a>
-
+          <FrameThree className="absolute bottom-0 left-0 hover:brightness-125 duration-150" />
           <a
             href="https://www.linkedin.com/in/buddhika-perera/"
             target="_blank"
+            className={cn(
+              titilium.className,
+              "flex items-center gap-1 z-[2]",
+              "absolute bottom-[5%] left-[45%] transform -translate-x-[45%] -translate-y-[5%] group opacity-50 hover:opacity-100 duration-150"
+            )}
           >
-            <FrameThree className="absolute bottom-0 left-0 hover:brightness-125 duration-150" />
+            <Image
+              src={"/assets/team/linkedin-icon.png"}
+              alt={"LinkedIn"}
+              height={18.75}
+              width={18.75}
+            />{" "}
+            LinkedIn
+            <Image
+              src={"/assets/team/arrow-right.png"}
+              alt={"LinkedIn"}
+              height={18.75}
+              width={18.75}
+              className="ml-1 group-hover:ml-3 duration-300"
+            />{" "}
           </a>
-
+          <FrameFour className="absolute bottom-0 -right-[2%] hover:brightness-125 duration-150" />
           <a
             href="https://www.linkedin.com/in/ashley-preen-8544661a1/"
             target="_blank"
+            className={cn(
+              titilium.className,
+              "flex items-center gap-1 z-[2]",
+              "absolute bottom-[5%] right-[2%] transform -translate-x-[2%] -translate-y-[5%] group opacity-50 hover:opacity-100 duration-150"
+            )}
           >
-            <FrameFour className="absolute bottom-0 -right-[2%] hover:brightness-125 duration-150" />
+            <Image
+              src={"/assets/team/linkedin-icon.png"}
+              alt={"LinkedIn"}
+              height={18.75}
+              width={18.75}
+            />{" "}
+            LinkedIn
+            <Image
+              src={"/assets/team/arrow-right.png"}
+              alt={"LinkedIn"}
+              height={18.75}
+              width={18.75}
+              className="ml-1 group-hover:ml-3 duration-300"
+            />{" "}
           </a>
         </div>
 
